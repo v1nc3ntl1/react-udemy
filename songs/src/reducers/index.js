a18d4d0a042c481f367c7aa01a147589bce4fe2b
vincent li <v1nc3ntl1@gmail.com>
2018-12-20 17:34:21 +1100

    remaining code to complete the section 13

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -9,7 +9,7 @@ const songsReducer = () => {
 };
 
 const selectedSongReducer = (selectedSong = null, action) => {
-    if (action === 'SONG_SELECTED'){
+    if (action.type === 'SONG_SELECTED'){
         return action.payload;
     }
 
